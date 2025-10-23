1cd854c82b7f8e18e88a665c778dbcf55dcf856f
DBertelliA <useromegad@gmail.com>
2025-10-23 13:19:41 +0200

    v.0.2 dialogo y demas cositas commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hola soy daniel");
+        Scanner sc = new Scanner(System.in);
+    
+        System.out.println("Hola soy daniel, como te llamas");
+        String mensaje = sc.nextLine();
+        System.out.println("- " + mensaje);
+        System.out.println("encantado de conocerte "+ mensaje);
+
     }
 }
