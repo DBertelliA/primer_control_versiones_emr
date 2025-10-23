15f8190e8648be8022648a7adce7bd1ea20b758f
DBertelliA <useromegad@gmail.com>
2025-10-23 13:44:02 +0200

    v0.3 version final de lo que he escrito + documentado

--- src/App.java
+++ src/App.java
@@ -2,11 +2,15 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-    
         System.out.println("Hola soy daniel, como te llamas");
         String mensaje = sc.nextLine();
         System.out.println("- " + mensaje);
         System.out.println("encantado de conocerte "+ mensaje);
+        /**documetar
+         * @author = DanielB
+         * @version = final
+        **/
+
 
     }
 }
